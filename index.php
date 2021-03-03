64d8953ae25d6766a6090b5511240e50fa83b94e
faridiqmal44 <faridiqmal44@gmail.com>
2021-03-04 01:03:52 +0700

    menambahkan list nama

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 </head>
 <body>
 	<h1>Coba git remote 2</h1>
+	<ul>
+		<li>Farid iqmal hakim</li>
+	</ul>
 	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio minus facilis autem asperiores exercitationem, quidem sed similique vel eius blanditiis magnam repudiandae ut, tenetur accusantium esse molestias modi aliquam officiis.</p>
 </body>
 </html>
