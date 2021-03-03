fce76b76ed4d1b6a266a7e7230dc44f7e7b7326e
faridiqmal44 <faridiqmal44@gmail.com>
2021-03-04 00:58:54 +0700

    menambahkan paragrap

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 	<h1>Coba git remote 2</h1>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio minus facilis autem asperiores exercitationem, quidem sed similique vel eius blanditiis magnam repudiandae ut, tenetur accusantium esse molestias modi aliquam officiis.</p>
 </body>
 </html>
